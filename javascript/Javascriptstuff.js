8af4ab6270a7bbf9a1c4d645aabe5f9fe2d68f2c
WhitebirdStrike <144002849+WhitebirdStrike@users.noreply.github.com>
2023-09-18 15:59:09 +0200

    Fix på navbar
    Holdt på med å fixe navbar, må fortsette å få det bedre, holdt på med å skrive mer tekst. Benyttet meg av chatgbt for første gang, var veldig nyttig. Bruk det hvis jeg står veldig fast på noe.

--- javascript/Javascriptstuff.js
+++ javascript/Javascriptstuff.js
@@ -1,55 +1,28 @@
-// First way to do it
-function toggleMe() {
-  let toggleStatus = document.getElementById("imDaToggle");
-  toggleStatus.classList.toggle("hideMe");
-}
-
-// Second way to do it
-function toggleMeTwo() {
-  // let toggleStatus = document.getElementById("imDaToggle");
-  // if (toggleStatus.classList.contains("hideMe")) {
-  //   toggleStatus.classList.remove("hideMe");
-  //   toggleStatus.classList.add("showMe");
-  // } else {
-  //   toggleStatus.classList.remove("showMe");
-  //   toggleStatus.classList.add("hideMe");
-  // }
-}
-
-// Third way to do it
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-document.getElementById("bossButton").onclick = function () {
-  myFunction();
-};
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches("bossButton")) {
-    let myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains("show")) {
-      myDropdown.classList.remove("show");
-    }
+function toggleDropdown(dropdownId) {
+  const dropdown = document.getElementById(dropdownId);
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    closeAllDropdowns();
+    dropdown.style.display = "block";
   }
-};
-
-function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show2");
 }
 
-document.getElementById("bossButton2").onclick = function () {
-  myFunction2();
-};
+function closeAllDropdowns() {
+  const dropdowns = document.querySelectorAll(
+    ".dropdown-content, .dropdown-content2"
+  );
+  dropdowns.forEach(function (dropdown) {
+    dropdown.style.display = "none";
+  });
+}
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdowns if the user clicks outside of them
 window.onclick = function (event) {
-  if (!event.target.matches("bossButton2")) {
-    let myDropdown = document.getElementById("myDropdown2");
-    if (myDropdown.classList.contains("show2")) {
-      myDropdown.classList.remove("show2");
-    }
+  if (
+    !event.target.matches(".bossButton") &&
+    !event.target.matches(".bossButton2")
+  ) {
+    closeAllDropdowns();
   }
 };
