41a961e73c0d04b5ffc254e853c00a7144151a35
WhitebirdStrike <144002849+WhitebirdStrike@users.noreply.github.com>
2023-09-20 15:59:09 +0200

    Dropdowns
    Har fortsatt med å fixe på dropdowns, har lagt til en ny flexbox med tekst og bilde. Må arbeide med å få javascript til å få bilde større når man klikker på det

---
+++ javascript/backup.js
@@ -0,0 +1,55 @@
+// First way to do it
+function toggleMe() {
+  let toggleStatus = document.getElementById("imDaToggle");
+  toggleStatus.classList.toggle("hideMe");
+}
+
+// Second way to do it
+function toggleMeTwo() {
+  // let toggleStatus = document.getElementById("imDaToggle");
+  // if (toggleStatus.classList.contains("hideMe")) {
+  //   toggleStatus.classList.remove("hideMe");
+  //   toggleStatus.classList.add("showMe");
+  // } else {
+  //   toggleStatus.classList.remove("showMe");
+  //   toggleStatus.classList.add("hideMe");
+  // }
+}
+
+// Third way to do it
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+document.getElementById("bossButton").onclick = function () {
+  myFunction();
+};
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches("bossButton")) {
+    let myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+document.getElementById("bossButton2").onclick = function () {
+  myFunction2();
+};
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches("bossButton2")) {
+    let myDropdown = document.getElementById("myDropdown2");
+    if (myDropdown.classList.contains("show2")) {
+      myDropdown.classList.remove("show2");
+    }
+  }
+};
